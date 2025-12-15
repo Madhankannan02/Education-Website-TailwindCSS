2acda6e9a9ae1f5c287a4c1bb026c1a66182a77c
Madhankannan02 <madhankannan29@gmail.com>
2025-12-15 19:10:56 +0530

    Completed the Navigation Bar with the whole responsiveness and the menu button functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,60 @@
-import { RiCloseLine } from '@remixicon/react';
+import { useState } from 'react';
+import { RiCloseLine, RiMenuLine } from '@remixicon/react';
+import { navItems } from '../constant/data';
 
 export function Header(){
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () =>{
+        setIsOpen(!isOpen);
+    }
+
     return(
-        <>
-            <div className="container">
+        <header className='w-full py-5'>
+            <div className="container flex item-center justify-between border-b border-b-white-95 pb-5 ">
                 <a href="#">
                     <img src="assets/images/logo.png" alt="Logo" width={170} height={50}
                     />
                 </a>
 
                 {/* Mobile Menu*/}
-                <nav>
+                <nav className={`navbar ${isOpen ? "active" : "" }`}>
                     {/* Close Menu */}
-                    <button>
+                    <button className='absolute top-8 right-8' onClick={handleClick}>
                         <RiCloseLine size={30} />
                     </button>
 
-                    <ul>
-                        li
+                    <ul className='space-y-5 text-center'>
+                        {navItems.map((item)=>(
+                            <li key={item.id}>
+                                <a href="#" className='text-lg font-medium hover:text-orange-50
+                                transition-colors'>{item.label}</a>
+                            </li>
+                        ))}
                     </ul>
+
+                    {/* Login Button */}
+                    <button className='primary-btn mt-12 max-w-40 w-full'>
+                        Login
+                    </button>
                 </nav>
+
+                {/* Menu Button */}
+                <button className='lg:hidden' onClick={handleClick}>
+                    <RiMenuLine />
+                </button>
+
+                <div className="max-lg:hidden flex items-center gap-10">
+                    <ul className='flex gap-10'>
+                        {navItems.map((item)=>(
+                            <li key={item.id}>
+                                <a href="#" className='hover:text-orange-50 transition-colors font-medium text-lg'>{item.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+
+                    <button className='max-lg:hidden primary-btn'>Login</button>
+                </div>
             </div>
-        </>
+        </header>
     );
 }
