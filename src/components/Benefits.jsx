94503055f87f50fe268163c97c63fd3d94bc5b20
Madhankannan02 <madhankannan29@gmail.com>
2025-12-18 20:18:23 +0530

    Completed the animation for the Benefits section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -17,7 +17,7 @@ export function Benefits() {
 
         <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 mt-12 lg:mt-16">
           {benefitItems.map((item) => (
-            <div className="bg-white p-10 flex flex-col rounded-xl">
+            <motion.div variants={variants.fadeInUp} className="bg-white p-10 flex flex-col rounded-xl">
               <div className="bg-orange-75 w-[55%] h-24 flex items-center justify-center mx-auto rounded-xl mb-8">
                 <img src={`assets/${item.icon}`} alt={item.title} width={64} height={64} />
               </div>
@@ -30,7 +30,7 @@ export function Benefits() {
               <button className="mt-auto ml-auto border border-white-95 w-14 h-14 flex items-center justify-center rounded-md text-orange-50 transition-colors hover:bg-orange-50 hover:text-white">
                 <RiArrowRightUpLine size={26} />
               </button>
-            </div>
+            </motion.div>
           ))}
         </div>
       </motion.div>
