5db9daf863fd49cc982d9b6e5663d24d8c7b2ec4
Madhankannan02 <madhankannan29@gmail.com>
2025-12-18 20:02:42 +0530

    Added the animation to the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,44 @@
 import { heroLogos } from "../constant/data";
 import { RiPlayFill } from "@remixicon/react";
 import Marquee from "react-fast-marquee";
+import { motion } from "motion/react";
+import * as variants from '../motion'
 
 export function Hero() {
 
  return (
   <section>
-   <div className="container">
+   <motion.div
+    variants={variants.staggerContainer}
+    initial='hidden'
+    whileInView='show'
+    viewport={{ once: true }}
+    className="container">
     <div className="mt-[50px] md:mt-20 text-center">
      <div className="relative max-w-max mx-auto pt-8 pl-8">
-      <span className="absolute top-0 left-0">
+      <motion.span variants={variants.fadeInUp} className="absolute top-0 left-0">
        <img src="assets/images/shape-1.png" alt="Title Shape" width={39} height={43} />
-      </span>
+      </motion.span>
 
-      <div className="flex items-center bg-white-99 border border-white-95 rounded-lg p-3.5 justify-centergap-2.5 max-w-max mx-auto flex-wrap text-center">
+      <motion.div
+      variants={variants.fadeInUp}
+      className="flex items-center bg-white-99 border border-white-95 rounded-lg p-3.5 justify-centergap-2.5 max-w-max mx-auto flex-wrap text-center">
        <span>
         <img src="assets/images/shape-2.png" alt="Title Shape" width={48} height={48} />
        </span>
        <h1 className="text-2xl md:text-4xl">
         <span className="text-orange-50">Unlock</span> Your Creative Potential
        </h1>
-      </div>
+      </motion.div>
      </div>
 
-     <p className="text-2xl font-medium mt-4 md:text-[28p]">with Online Design and Development Courses.</p>
-     <p>Learn from Industry Experts and Enhance Your Skills.</p>
+     <motion.p variants={variants.fadeInUp}
+     className="text-2xl font-medium mt-4 md:text-[28p]">with Online Design and Development Courses.</motion.p>
+     <motion.p variants={variants.fadeInUp}>Learn from Industry Experts and Enhance Your Skills.</motion.p>
 
      <div className="flex items-center justify-center gap-3 mt-12 flex-wrap">
-      <button className="primary-btn max-sm:w-[80%]">Explore Courses</button>
-      <button className="secondary-btn max-sm:w-[80%]">View Pricing</button>
+      <motion.button variants={variants.fadeInUp} className="primary-btn max-sm:w-[80%]">Explore Courses</motion.button>
+      <motion.button variants={variants.fadeInUp} className="secondary-btn max-sm:w-[80%]">View Pricing</motion.button>
      </div>
 
      <div className="mt-8 lg:mt-[100px] relative overflow-hidden">
@@ -47,7 +57,7 @@ export function Hero() {
     </div>
 
     <figure className="relative rounded-xl overflow-hidden mt-10 md:mt-14 lg:mt-[50px] max-w-[920px] w-full h-[500px] mx-auto">
-     <img src="assets/images/hero-banner.png" alt="hero-banner" width={940} height={500} className="w-full h-full object-cover"/>
+     <img src="assets/images/hero-banner.png" alt="hero-banner" width={940} height={500} className="w-full h-full object-cover" />
 
      <div className="absolute inset-0 bg-black/20 z-10" />
 
@@ -57,7 +67,7 @@ export function Hero() {
       </span>
      </div>
     </figure>
-   </div>
-  </section>
+   </motion.div>
+  </section >
  );
 }
